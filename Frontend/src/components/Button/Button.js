6c040a39734deb2b33ddd0093456da15382f38c0
Toksik <toksamal2004@gmail.com>
2024-09-09 16:01:47 +0300

    update js

--- Frontend/src/components/Button/Button.js
+++ Frontend/src/components/Button/Button.js
@@ -1,9 +1,8 @@
 import Link from "next/link";
-import styles from "../styles/A.module.css"
 
 export default function A({text, href}){
     return (
-        <Link href={href} className={styles.link}>
+        <Link href={href}>
            {text}
         </Link>
     )
--- Frontend/src/pages/index.js
+++ Frontend/src/pages/index.js
@@ -1,16 +1,13 @@
-import Link from 'next/link';
-import A from "../components/A"
-import styles from '../styles/Menu.module.css'
+
 
 const Index = () => {
     return (
         <>
-            <div className={styles.navbar}>
-                <A href={'/'} text="Main"></A>
-                <A href={'/products'} text="Shop"></A>
+            <div>
+                <h1>Hello</h1>
             </div>
             <div>
-                <h1 className={styles.h1}>Main page</h1>
+                <h1>Main page</h1>
             </div>
         </>
     )

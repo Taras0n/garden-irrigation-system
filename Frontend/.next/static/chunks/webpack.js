93181a63f57b346c9bbf8b86a3d606d570c19ad6
Taras0n <mujutyuk@gmail.com>
2024-09-09 16:16:27 +0200

    index

--- Frontend/.next/static/chunks/webpack.js
+++ Frontend/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a371228e3ece71bc"; }
+/******/ 		__webpack_require__.h = function() { return "1bcf916645becd1e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
